00ff6b3569bd389c99d94abb2e4399dfd0ebbf9f
Pavel <foujeupavel@gmail.com>
2019-11-22 20:06:35 +0100

    adding description UI

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -13,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setup();
+    }
+
+    void setup() {
+
 
     }
 
